c36574efc9b9cfcf5dd118b5459d9dfe06c7ca2b
jaforbes <james.a.forbes@gmail.com>
2020-04-19 21:27:19 +1000

    ...

--- gestures.js
+++ gestures.js
@@ -3,10 +3,14 @@ import H from './web_modules/hammerjs.js'
 export default function gestures(container, visitor){
 	const mc = new H(document.body)
 
-	mc.on("tap press swipe pan", visitor)
+	mc.on("press swipe pan", visitor)
 
 	mc.get('pan').set({ direction: H.DIRECTION_ALL })
 	mc.get('swipe').set({ direction: H.DIRECTION_ALL })
 
+	// faster than a tap
+	window.addEventListener('click', e => {
+		visitor({ type: 'click', center: { x: e.x, y: e.y } })
+	})
 	return mc
 }
