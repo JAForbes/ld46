bf22b2f3667c13bfcef2701b690358b8642ef9ba
jaforbes <james.a.forbes@gmail.com>
2020-04-20 08:11:12 +1000

    ...

--- index.js
+++ index.js
@@ -23,6 +23,40 @@ css.$animate.out = (time, styles) => ({ dom }) => () => new Promise(res => {
 
 window.oncontextmenu = e => e.preventDefault()
 
+function relativeGestureService({ stream, screenDimensions, gesture }){
+
+	const relativeGesture = stream.of()
+	const halfDimensions = screenDimensions.actual.map(
+		({ width, height }) => ({ width: width / 2, height: height / 2 })
+	)
+
+	gesture.map( x =>
+		(screenDimensions.actual().orientation == 'landscape' || true)
+		? {
+			type: x.type
+			, x: x.center.x - halfDimensions().width
+			, y: x.center.y - halfDimensions().height
+			, scale: x.scale
+
+		}
+		: {
+			type: x.type
+			,x: x.center.y - halfDimensions().height
+			,y: x.center.x - halfDimensions().width
+			, scale: x.scale
+		}
+	)
+	.map(
+		({ type, x, y, scale }) => ({
+			type, x, y, scale, theta:
+				(Math.atan2(y,x) + Math.PI / 2)
+		})
+	)
+	.map( relativeGesture )
+
+	return relativeGesture
+}
+
 function App({ v, route: parent, stream }){
 
 	const state = {}
@@ -68,34 +102,9 @@ function App({ v, route: parent, stream }){
 		lastGesture(x)
 	})
 
-	const relativeGesture = stream()
-	const halfDimensions = screenDimensions.actual.map(
-		({ width, height }) => ({ width: width / 2, height: height / 2 })
-	)
-
-	lastGesture.map( x =>
-		(screenDimensions.actual().orientation == 'landscape' || true)
-		? {
-			type: x.type
-			, x: x.center.x - halfDimensions().width
-			, y: x.center.y - halfDimensions().height
-			, scale: x.scale
-
-		}
-		: {
-			type: x.type
-			,x: x.center.y - halfDimensions().height
-			,y: x.center.x - halfDimensions().width
-			, scale: x.scale
-		}
-	)
-	.map(
-		({ type, x, y, scale }) => ({
-			type, x, y, scale, theta:
-				(Math.atan2(y,x) + Math.PI / 2)
-		})
-	)
-	.map( relativeGesture )
+	const relativeGesture = relativeGestureService({
+		stream, screenDimensions, gesture: lastGesture
+	})
 
 	const cameraEl = A.stream.of()
 	const cameraScale = A.stream.of(1)
